2fc4469ff5e32a811ca08cae0d2c1869e3fae0b7
MefitHp <mefitDev@gmail.com>
2018-10-02 23:36:19 -0500

    Fixed SelectInput / TextInput components

--- src/components/common/SelectInput.js
+++ src/components/common/SelectInput.js
@@ -5,15 +5,17 @@ const SelectInput = ({ name, label, onChange, defaultOption, value, error, optio
         <div className="form-group">
             <label htmlFor={name}>{label}</label>
             <div className="field">
-                <Select
+                <select
                     name={name}
+                    value={value}
                     onChange={onChange}
                     className="form-control">
                     <option value="">{defaultOption}</option>>
-            {options.map(option => {
-                        return (<option key={option.value} value={option.value}>{option.text}</option>)
-                    })}
-                </Select>
+                    {options.map(option => {
+                        return <option key={option.value} value={option.value}>{option.text}</option>
+                    })
+                    }
+                </select>
                 {error && <div className="alert alert-danger">{error}</div>}
             </div>
         </div>
